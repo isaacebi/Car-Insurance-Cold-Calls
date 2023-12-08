cf88a56aae531469044119a3ba06ca12782ce26d
isaacebi <isaac_ebi@rocketmail.com>
2023-12-08 21:26:58 +0800

    Update feature_selection_pipeline.py
    coding debt

--- src/data/feature_selection_pipeline.py
+++ src/data/feature_selection_pipeline.py
@@ -46,6 +46,7 @@ def cramers_corrected_stat(label, target):
     kcorr = k - ((k-1)**2)/(n-1)
     return np.sqrt(phi2corr / min( (kcorr-1), (rcorr-1)))
 
+# manual filter, if not needed can proceed using scikit-learn pipeline
 class col_filter:
     def __init__(self, df, target, continous_list, categorical_list):
         self.df = df
@@ -53,11 +54,13 @@ class col_filter:
         self.conti_col = continous_list
         self.categ_col = categorical_list
     
-    def con_filter(self):
+    # create continous filter for input continous, output categorical
+    def con_filter(self) -> dict:
         scores = {}
         
         for col in self.conti_col:
             if self.df[col].dtypes != '<M8[ns]':
+                # using Logistic Regression to get scores
                 lr = LogisticRegression()
                 # fit model
                 lr.fit(
@@ -74,15 +77,18 @@ class col_filter:
             
         return pd.DataFrame.from_dict(scores, orient='index', columns=['Score'])
         
+    # create continous filter for input categorical, output categorical
     def cat_filter(self):
         scores = {}
         for col in self.categ_col:
+            # using Crammer's V
             score = cramers_corrected_stat(self.df[col], self.df[self.target])
             scores[col] = score
         
         return pd.DataFrame.from_dict(scores, orient='index', columns=['Score'])
     
-    def get_columns(self, threshold):
+    # output selected features/labels
+    def get_columns(self, threshold:float) -> list:
         labels_score = pd.concat([self.con_filter(), self.cat_filter()])
         col_selected = labels_score[labels_score['Score'] >= threshold].index.tolist()
         return col_selected
