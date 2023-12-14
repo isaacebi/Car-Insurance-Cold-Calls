ac75efa8413f4a9eff8097b7a36372e440f045bf
isaacebi <isaac_ebi@rocketmail.com>
2023-12-14 21:08:51 +0800

    Update data_scores.py

--- src/data/data_scores.py
+++ src/data/data_scores.py
@@ -1,12 +1,15 @@
 # %% Importing libraries
 import os
+import shap
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.preprocessing import LabelEncoder
-
 from xgboost import XGBClassifier
 from xgboost import plot_importance
+
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 # %% Pathing
 CURRENT_DIR = os.getcwd()
@@ -20,34 +23,58 @@ TRAIN_PATH = os.path.join(DATA_RAW_PATH, 'carInsurance_train.csv')
 from process_pipeline import process_data
 
 # %%
-df = pd.read_csv(TRAIN_PATH)
-df = process_data(df)
+def get_features_score():
+    df = pd.read_csv(TRAIN_PATH)
+    df = process_data(df)
 
-X = df.drop(columns=['CarInsurance'])
-y = df['CarInsurance']
+    X = df.drop(columns=['CarInsurance'])
+    y = df['CarInsurance']
+    
+    # some of dtypes in X is still in obj
+    for col in X:
+        # encoded object then scaling
+        if X[col].dtype == 'O':
+            X[col] = LabelEncoder().fit_transform(X[col])
+            X[col] = StandardScaler().fit_transform(np.expand_dims(X[col], axis=-1))
 
-# some of dtypes in X is still in obj
-for col in X:
-    if X[col].dtype == 'O':
-        X[col] = LabelEncoder().fit_transform(X[col])
+        # scaling numeric
+        else:
+            X[col] = StandardScaler().fit_transform(np.expand_dims(X[col], axis=-1))
 
-# %%
-# define model
-model = XGBClassifier()
+    # splitting data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-# fit the model
-model.fit(X, y)
+    # define model
+    model = XGBClassifier()
 
-# get importance
-importance = model.feature_importances_
+    # fit the model
+    model.fit(X_train, y_train)
 
-# create pandas scores
-score = pd.DataFrame(importance, index=X.columns.tolist(), columns=['Score'])
+    # using shap
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X_test)
 
-score.plot.bar(y='Score')
-plt.show()
+    # visualize feature importance
+    shap.summary_plot(shap_values, X_test, plot_type='bar')
+    plt.show()
 
-plot_importance(model)
-plt.show()
+    shap.summary_plot(shap_values, X_test)
+    plt.show()
+
+    plt.barh(X.columns.tolist(), model.feature_importances_)
+    plt.show()
+
+# %% This section need to be further explore in the future
+# # define model
+# model = XGBClassifier().fit(X, y)
+
+# # explain the model's predictions using SHAP
+# # (same syntax works for LightGBM, CatBoost, scikit-learn, transformers, Spark, etc.)
+# explainer = shap.Explainer(model)
+# shap_values = explainer(X)
+
+# # visualize the first prediction's explanation
+# shap.plots.waterfall(shap_values[0])
+# shap.plots.waterfall(shap_values[2])
 
 # %%
